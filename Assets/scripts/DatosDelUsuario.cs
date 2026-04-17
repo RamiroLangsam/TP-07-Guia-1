56a329c6aee1d958544103ed580de41a018c8ca7
RamiroLangsam <ramirolangsam@gmail.com>
2026-04-17 09:01:37 -0300

    Initial commit

---
+++ Assets/scripts/DatosDelUsuario.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    string nombre = "ramiro";
+    int edad = 15;
+    float estatura = 177;
+    bool esDonante = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string mensajeDonante;
+        if (esDonante) {
+            mensajeDonante = "y soy donante ";
+        }
+        else {
+            mensajeDonante = "No soy donante";
+        }
+
+
+        Debug.Log("mi nombre es " + nombre + ", tengo" + edad + "mi estatura es " + estatura + mensajeDonante);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int variableUno = 2;
+    float variableDos = 4;
+    string varibleTres = "Hola como estas";
+    bool variableCuatro = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log(variableUno);
+      Debug.Log(variableDos);
+       Debug.Log(varibleTres);
+        Debug.Log(variableCuatro);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
